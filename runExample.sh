0e5743d614e8c966fbc69830a0a3353f2c0383df
Kim Lindberg <kim.lindberg@tecnotree.com>
2015-06-15 09:26:55 +0300

    Fixed script to allow spaces in nickname

--- runExample.sh
+++ runExample.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-java -cp target/dependency/*:target/whatsapi4j-1.0.1-SNAPSHOT.jar net.sumppen.whatsapi4j.example.ExampleApplication $*
+USER=$1
+PASSWORD=$2
+IDENTITY=$3
+NICK=$4
+
+java -cp target/dependency/*:target/whatsapi4j-1.0.1-SNAPSHOT.jar net.sumppen.whatsapi4j.example.ExampleApplication $USER "$PASSWORD" "$IDENTITY" "$NICK"
